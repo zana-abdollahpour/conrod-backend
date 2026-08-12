1cf6ed84c9f8332462e62e7d518a41bd123817d6
Zana Abdollahpour <zana.ap4@gmail.com>
2026-08-12 13:34:27 +0330

    use config module in main.ts files

--- src/env/env.config.ts
+++ src/env/env.config.ts
@@ -1,10 +1,12 @@
 import Joi from 'joi';
 
 export const envValidationSchema = Joi.object({
+  APP_PORT: Joi.number().port().required(),
+
   DATASOURCE_USERNAME: Joi.required(),
   DATASOURCE_PASSWORD: Joi.required(),
   DATASOURCE_HOST: Joi.required(),
-  DATASOURCE_PORT: Joi.number().required(),
+  DATASOURCE_PORT: Joi.number().port().required(),
   DATASOURCE_DATABASE: Joi.required(),
   DATASOURCE_URL: Joi.required(),
 });
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('APP_PORT');
+
+  await app.listen(port ?? 3000);
 }
 
 void bootstrap();
