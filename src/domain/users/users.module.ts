2afffe770caae96f1ba073a71289d91672a402f4
Zana Abdollahpour <zana.ap4@gmail.com>
2026-08-12 10:07:00 +0330

    implement users services

--- src/domain/users/users.module.ts
+++ src/domain/users/users.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { User } from 'domain/users/entities/user.entity';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
   providers: [UsersService],
 })
