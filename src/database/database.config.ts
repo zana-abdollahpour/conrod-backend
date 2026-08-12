434daa76b187e32ff8636746e498ad4718d251b2
Zana Abdollahpour <zana.ap4@gmail.com>
2026-08-12 13:21:29 +0330

    implement alternative method for db config

--- src/database/database.config.ts
+++ src/database/database.config.ts
@@ -1,5 +1,12 @@
 import { registerAs } from '@nestjs/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-export default registerAs('database', () => ({
-  url: process.env.DATASOURCE_URL,
-}));
+export default registerAs(
+  'database',
+  () =>
+    ({
+      type: 'postgres',
+      url: process.env.DATASOURCE_URL,
+      autoLoadEntities: true,
+    }) as const satisfies TypeOrmModuleOptions,
+);
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,19 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigType } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import databaseConfig from 'database/database.config';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule.forFeature(databaseConfig)],
-      inject: [databaseConfig.KEY],
-      useFactory: (dbConfig: ConfigType<typeof databaseConfig>) => ({
-        type: 'postgres',
-        url: dbConfig.url,
-        autoLoadEntities: true,
-      }),
-    }),
-  ],
+  imports: [TypeOrmModule.forRootAsync(databaseConfig.asProvider())],
 })
 export class DatabaseModule {}
