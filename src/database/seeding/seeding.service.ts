0f3229db0ca245e97924b374b61f210a3c9c852a
Zana Abdollahpour <zana.ap4@gmail.com>
2026-08-13 19:01:45 +0330

    add seeding resource and functionality

---
+++ src/database/seeding/seeding.service.ts
@@ -0,0 +1,137 @@
+import { Injectable } from '@nestjs/common';
+import { DataSource } from 'typeorm';
+
+import { Category } from 'domain/categories/entities/category.entity';
+import { OrderItem } from 'domain/orders/entities/order-item.entity';
+import { Order } from 'domain/orders/entities/order.entity';
+import { OrderStatus } from 'domain/orders/enums/order-status.enum';
+import { Payment } from 'domain/payments/entities/payment.entity';
+import { Product } from 'domain/product/entities/product.entity';
+import { User } from 'domain/users/entities/user.entity';
+
+@Injectable()
+export class SeedingService {
+  constructor(private readonly dataSource: DataSource) {}
+
+  async seed() {
+    await this.dataSource.transaction(async (manager) => {
+      const usersRepository = manager.getRepository(User);
+      const categoriesRepository = manager.getRepository(Category);
+      const productsRepository = manager.getRepository(Product);
+      const ordersRepository = manager.getRepository(Order);
+      const orderItemsRepository = manager.getRepository(OrderItem);
+      const paymentsRepository = manager.getRepository(Payment);
+
+      const orders = await ordersRepository.find();
+      await ordersRepository.remove(orders);
+      const users = await usersRepository.find();
+      await usersRepository.remove(users);
+      const products = await productsRepository.find();
+      await productsRepository.remove(products);
+      const categories = await categoriesRepository.find();
+      await categoriesRepository.remove(categories);
+
+      const cat1 = categoriesRepository.create({ name: 'Electronics' });
+      const cat2 = categoriesRepository.create({ name: 'Books' });
+      const cat3 = categoriesRepository.create({ name: 'Computers' });
+      const cat4 = categoriesRepository.create({ name: 'Games' });
+
+      await categoriesRepository.save([cat1, cat2, cat3, cat4]);
+
+      const p1 = productsRepository.create({
+        name: 'Book of Cain',
+        description:
+          'The writings of an elderly scholar about this perilous world.',
+        price: 102.5,
+        categories: [cat2],
+      });
+      const p2 = productsRepository.create({
+        name: 'Smart TV',
+        price: 2350,
+        categories: [cat1, cat3],
+      });
+      const p3 = productsRepository.create({
+        name: 'Macbook Pro',
+        price: 1200,
+        categories: [cat3],
+      });
+      const p4 = productsRepository.create({
+        name: 'Gaming PC',
+        description: 'Latest generation hardware for the best experience.',
+        price: 2000,
+        categories: [cat3],
+      });
+      const p5 = productsRepository.create({
+        name: 'Game Mechanics: Advanced Game Design',
+        description: 'Learn how to craft well-designed game mechanics.',
+        price: 149.9,
+        categories: [cat2],
+      });
+      const p6 = productsRepository.create({
+        name: 'Warcraft III: Reign of Chaos',
+        description: 'A true classic in the RTS genre.',
+        price: 25.99,
+        categories: [cat4],
+      });
+
+      await productsRepository.save([p1, p2, p3, p4, p5, p6]);
+
+      const u1 = usersRepository.create({
+        name: 'Pedro Faria',
+        email: 'jarulf@mail.com',
+        phone: '988888888',
+        password: '123456',
+      });
+      const u2 = usersRepository.create({
+        name: 'Chris Metzen',
+        email: 'chris@blizz.com',
+        phone: '977777777',
+        password: '654321',
+      });
+
+      await usersRepository.save([u1, u2]);
+
+      const oi1 = orderItemsRepository.create({
+        product: p1,
+        quantity: 2,
+        price: p1.price,
+      });
+      const oi2 = orderItemsRepository.create({
+        product: p3,
+        quantity: 1,
+        price: p3.price,
+      });
+      const oi3 = orderItemsRepository.create({
+        product: p3,
+        quantity: 2,
+        price: p3.price,
+      });
+      const oi4 = orderItemsRepository.create({
+        product: p5,
+        quantity: 2,
+        price: p5.price,
+      });
+
+      const pay1 = paymentsRepository.create();
+
+      const o1 = ordersRepository.create({
+        customer: u1,
+        items: [oi1, oi2],
+        status: OrderStatus.AWAITING_SHIPMENT,
+        payment: pay1,
+      });
+      const o2 = ordersRepository.create({
+        customer: u2,
+        items: [oi3],
+        status: OrderStatus.AWAITING_SHIPMENT,
+      });
+      const o3 = ordersRepository.create({
+        customer: u1,
+        items: [oi4],
+        status: OrderStatus.AWAITING_SHIPMENT,
+      });
+
+      await ordersRepository.save([o1, o2, o3]);
+    });
+  }
+}
