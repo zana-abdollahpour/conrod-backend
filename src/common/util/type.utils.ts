e5e3573080082a449172ca2bcfe19f291a17383a
Zana Abdollahpour <zana.ap4@gmail.com>
2026-08-16 17:08:19 +0330

    require at least one role in @Roles() decorator

---
+++ src/common/util/type.utils.ts
@@ -0,0 +1,1 @@
+export type NonEmptyArray<TArg> = [TArg, ...TArg[]];
--- src/iam/authorization/decorators/roles.decorator.ts
+++ src/iam/authorization/decorators/roles.decorator.ts
@@ -1,7 +1,9 @@
 import { SetMetadata } from '@nestjs/common';
+import { NonEmptyArray } from 'common/util/type.utils';
 
 import { Role } from 'iam/authorization/enum/roles.enum';
 
 export const ROLES_KEY = 'roles';
 
-export const Roles = (...roles: Role[]) => SetMetadata(ROLES_KEY, roles);
+export const Roles = (...roles: NonEmptyArray<Role>) =>
+  SetMetadata(ROLES_KEY, roles);
