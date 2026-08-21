d27b7143e8353449d78ca312132bbd0783d8ed33
Zana Abdollahpour <zana.ap4@gmail.com>
2026-08-21 17:45:57 +0330

    refactor: rename offset to page in pagination dtp, and apply max

--- src/querying/dto/pagination.dto.ts
+++ src/querying/dto/pagination.dto.ts
@@ -1,12 +1,16 @@
-import { IsOptional } from 'class-validator';
+import { IsOptional, Max } from 'class-validator';
+
 import { IsCardinal } from 'common/decorators/validators/is-cardinal.decorator';
+import { MAX_PAGE_NUMBER, MAX_PAGE_SIZE } from 'querying/querying.config';
 
 export class PaginationDto {
   @IsOptional()
   @IsCardinal()
+  @Max(MAX_PAGE_SIZE)
   limit?: number;
 
   @IsOptional()
   @IsCardinal()
-  offset?: number;
+  @Max(MAX_PAGE_NUMBER)
+  page?: number = 1;
 }
--- src/querying/querying.config.ts
+++ src/querying/querying.config.ts
@@ -4,3 +4,7 @@ export const DefaultPageSizes = {
   CATEGORIES: 30,
   PRODUCTS: 20,
 } as const satisfies Record<string, number>;
+
+export const MAX_PAGE_SIZE = 100;
+
+export const MAX_PAGE_NUMBER = 25;
